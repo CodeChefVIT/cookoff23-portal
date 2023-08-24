6de0c156e3abfa8df549b0fb98dc2c6b13264774
Pranav Deshmukh <pranavdeshmukh190@gmail.com>
2023-08-24 23:19:41 +0530

    Text editor code completed

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,24 +1,30 @@
 import Question from "@/components/questions";
 import questionData from "../../Dummy_Data";
 import Navbar from "@/components/Navbar";
+import TextEditor from "../components/textEditor";
 
 export default function Home() {
   return (
     <>
-      <main>
-        <Navbar />
-        <Question
-          objective={questionData.objective}
-          points={questionData.points}
-          task={questionData.task}
-          inputFormat={questionData.inputFormat}
-          outputFormat={questionData.outputFormat}
-          sampleInput1={questionData.sampleInput[0]}
-          sampleOutput1={questionData.sampleOutput[0]}
-          sampleInput2={questionData.sampleInput[1]}
-          sampleOutput2={questionData.sampleOutput[1]}
-          explanation={questionData.explanation}
-        />
+      <Navbar />
+      <main className="flex">
+        <div className="w-[40%]">
+          <Question
+            objective={questionData.objective}
+            points={questionData.points}
+            task={questionData.task}
+            inputFormat={questionData.inputFormat}
+            outputFormat={questionData.outputFormat}
+            sampleInput1={questionData.sampleInput[0]}
+            sampleOutput1={questionData.sampleOutput[0]}
+            sampleInput2={questionData.sampleInput[1]}
+            sampleOutput2={questionData.sampleOutput[1]}
+            explanation={questionData.explanation}
+          />
+        </div>
+        <div className="w-[60%]">
+          <TextEditor />
+        </div>
       </main>
     </>
   );
