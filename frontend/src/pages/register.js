6ace57cf03b67cb83dc1e68dc9399142b2ddc600
Aaditya Mahanta <aadityamahanta07@gmail.com>
2023-09-22 01:16:34 +0530

    fix: reg page

--- frontend/src/pages/register.js
+++ frontend/src/pages/register.js
@@ -29,14 +29,13 @@ const validate = (values) => {
 };
 
 function Register () {
-  const [error, setError] = useState(false);
+  const [error, setError] = useState("");
   const router = useRouter();
   const formik = useFormik({
     initialValues: {
       name: "",
       regNo: "",
-      email: "",
-      password: "1234"
+      email: ""
     },
     validate,
     onSubmit: async (values) => {
@@ -44,7 +43,7 @@ function Register () {
 
       try {
         const response = await axios.post(
-          `https://api-cookoff-prod.codechefvit.com/auth/signup`,
+          `https://api-cookoff-prod.codechefvit.com/auth/create`,
           values
         );
         console.log("API response:", response);
@@ -59,7 +58,7 @@ function Register () {
           const statusCode = error.response.status;
           console.log(`status code: ${statusCode}`);
           if (statusCode === 400) {
-            setError(true);
+            setError(error.response.data.error);
           } else {
             console.log("An error occurred:", error);
           }
@@ -88,19 +87,19 @@ function Register () {
           </div>
           {error && (
             <div className="text-[#D9D9D999] my-10 text-center">
-              Registration failed. Please check your information
+              {error}
             </div>
           )}
           <form
             className="w-[400px] self-center"
             onSubmit={formik.handleSubmit}
           >
-            <div className="mb-[40px]">
+            <div className="mb-6">
               <input
                 className="w-full py-[18px] px-[33px] text-[#D9D9D999] bg-[#1F1F1F] rounded-[25px] text-[22px] font-semibold"
                 id="name"
                 type="text"
-                placeholder="name"
+                placeholder="Name"
                 onChange={formik.handleChange}
                 value={formik.values.name}
               />
@@ -115,7 +114,7 @@ function Register () {
                 className="w-full py-[18px] px-[33px] text-[#D9D9D999] bg-[#1F1F1F] rounded-[25px] text-[22px] font-semibold"
                 id="regNo"
                 type="text"
-                placeholder="regNo"
+                placeholder="Registration Number"
                 onChange={formik.handleChange}
                 value={formik.values.regNo}
               />
@@ -130,7 +129,7 @@ function Register () {
                 className="w-full py-[18px] px-[33px] text-[#D9D9D999] bg-[#1F1F1F] rounded-[25px] text-[22px] font-semibold"
                 id="email"
                 type="email"
-                placeholder="email"
+                placeholder="Email"
                 onChange={formik.handleChange}
                 value={formik.values.email}
               />
@@ -145,7 +144,7 @@ function Register () {
                 className="uppercase text-[#D9D9D9] font-semibold py-[16px] px-[26px] text-[22px] border-[3px] border-[#D9D9D9] rounded-full hover:bg-[#D9D9D9] hover:text-black mt-5"
                 type="submit"
               >
-                Register to code!
+                Register to cook!
               </button>
             </div>
           </form>
