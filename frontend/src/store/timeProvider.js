10a09e56808e4bdf072397ff1c29a63a6e68acee
Aaditya Mahanta <aadityamahanta07@gmail.com>
2023-10-02 20:49:02 +0530

    cleanup

--- frontend/src/store/timeProvider.js
+++
@@ -1,9 +0,0 @@
-import { create } from "zustand";
-
-const useTimerStore = create((set) => ({
-  Time: 2*60*45,
-//   countdeduct: () => set((state) => ({ initalTime: state.initalTime - 1 })),
-//   countParse: () => set((state) => ({ initalTime: parseInt(state.initalTime) })),
-}));
-
-export default useTimerStore;
--- frontend/src/store/tokenProvider.js
+++
@@ -1,8 +0,0 @@
-import { create } from "zustand";
-
-const useTokenStore = create((set) => ({
-  access_token: "",
-  refresh_token: "",
-}));
-
-export default useTokenStore;
